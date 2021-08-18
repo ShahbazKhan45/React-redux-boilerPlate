4702e5cd9f4346a4b4fd58ddc03159a2b82b09a1
Shahbaz Khan <64363005+ShahbazKhan45@users.noreply.github.com>
2021-08-19 03:07:54 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React,{Component} from 'react';
-import Home from './containers/Home';
+import { React, Component } from 'react';
+import Home from './containers/home';
 import './App.css';
 
-class App extends Component{
-    render(){
-        return(
-            <div>
-                <Home /> 
-            </div>
-            
-        )
-    }
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>App Component</h1>
+        <Home />
+      </div>
+    )
+  }
 }
-export default App;
 
+export default App;
